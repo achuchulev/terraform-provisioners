e15b011cd2a685837c51c1f10ac53a7b5ea52638
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-02 17:35:37 +0300

    Create redis.sh

---
+++ provision/redis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# check if redis is installed
+# install redis if it's not
+
+# check if curl is installed
+which redis || {
+sudo apt-get update
+sudo apt-get install -y redis
+}
