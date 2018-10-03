e9368b2304d4d97051811f45560dc983e70f2ca6
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-10-03 10:17:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,9 +28,9 @@ resource "aws_instance" "atanasc-w1" {
   provisioner "remote-exec" {
     inline = [
       "sudo chmod +x /tmp/provision/redis.sh",
-      "sudo sh /tmp/provision/redis.sh",
-      "which redis-server",
-      "redis-cli ping"
+      "sudo sh /tmp/provision/redis.sh", # install redis
+      "which redis-server",              # verify installation
+      "redis-cli ping"                   # verify redis is working
     ]
   }
 }
